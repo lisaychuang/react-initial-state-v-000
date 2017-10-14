3b60fd80128e68be40186a71fe6e417094eae76f
Lisa Huang <lisa.yc.huang@gmail.com>
2017-10-14 16:45:04 -0700

    Created Address and ToggleButton components. Set initial state

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,21 @@
-// Component Code Goes Here
+import React from 'react';
+
+class Address extends React.Component {
+    constructor(props){
+        super(props);
+        
+        // define initial state
+        this.state = {}
+    }
+    
+    render(){
+        return (
+            <div className="address">
+                {this.props.street}, {this.props.city}
+            </div>  
+        );
+    }
+
+}
+
+export default Address;
